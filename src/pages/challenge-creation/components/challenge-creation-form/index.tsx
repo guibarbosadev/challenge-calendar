7ac67c45857cb8c27cb5c5d180adea8e6e6c318c
Guilherme Barbosa Pereira <guilherme.barbosa@outlook.com>
2021-07-01 20:00:36 -0300

    Stylish challenge-creation-form a little bit

--- src/pages/challenge-creation/components/challenge-creation-form/index.tsx
+++ src/pages/challenge-creation/components/challenge-creation-form/index.tsx
@@ -1,43 +1,43 @@
-import React from "react";
-import styles from "./styles.module.scss";
+import React from 'react';
+import styles from './styles.module.scss';
 
 export interface IValues {
-  name: string;
-  duration: string;
+    name: string;
+    duration: string;
 }
 
 export interface IProps {
-  handleSubmit: (values: IValues) => void;
+    handleSubmit: (values: IValues) => void;
 }
 
 export default function ChallengeCreationForm({ handleSubmit }: IProps) {
-  const [name, setName] = React.useState("");
-  const [duration, setDuration] = React.useState("");
+    const [name, setName] = React.useState('');
+    const [duration, setDuration] = React.useState('');
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    handleSubmit({ name, duration });
-  };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        handleSubmit({ name, duration });
+    };
 
-  return (
-    <form className={styles.wrapper} onSubmit={onSubmit}>
-      <div className={styles.field}>
-        <label htmlFor="name">What will be your daily challenge?</label>
-        <input
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div className={styles.field}>
-        <label htmlFor="duration">For how many days?</label>
-        <input
-          name="duration"
-          value={duration}
-          onChange={(e) => setDuration(e.target.value)}
-        />
-      </div>
-      <button type="submit">Finish</button>
-    </form>
-  );
+    return (
+        <form onSubmit={onSubmit}>
+            <div className={styles.field}>
+                <label htmlFor="name">What will be your daily challenge?</label>
+                <input
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className={styles.field}>
+                <label htmlFor="duration">For how many days?</label>
+                <input
+                    name="duration"
+                    value={duration}
+                    onChange={(e) => setDuration(e.target.value)}
+                />
+            </div>
+            <button type="submit">Finish</button>
+        </form>
+    );
 }
--- src/pages/challenge-creation/index.tsx
+++ src/pages/challenge-creation/index.tsx
@@ -1,22 +1,29 @@
-import React from "react";
-import { useChallengeStore } from "../../stores/challenge";
+import React from 'react';
+import { useChallengeStore } from '../../stores/challenge';
 import ChallengeCreationForm, {
-  IValues,
-} from "./components/challenge-creation-form";
-import { add, format } from "date-fns";
+    IValues
+} from './components/challenge-creation-form';
+import { add, format } from 'date-fns';
+import styles from './styles.module.scss';
 
 export default function ChallengeCreationPage() {
-  const challengeStore = useChallengeStore();
+    const challengeStore = useChallengeStore();
 
-  function handleSubmit({ name, duration }: IValues) {
-    const date = new Date();
-    const dateFormat = "dd-MM-yyyy";
-    const startDate = format(date, dateFormat);
-    const days = Math.round(Number(duration));
-    const endDate = format(add(date, { days }), dateFormat);
+    function handleSubmit({ name, duration }: IValues) {
+        const date = new Date();
+        const dateFormat = 'dd-MM-yyyy';
+        const startDate = format(date, dateFormat);
+        const days = Math.round(Number(duration));
+        const endDate = format(add(date, { days }), dateFormat);
 
-    challengeStore.createChallenge({ name, startDate, endDate });
-  }
+        challengeStore.createChallenge({ name, startDate, endDate });
+    }
 
-  return <ChallengeCreationForm handleSubmit={handleSubmit} />;
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <ChallengeCreationForm handleSubmit={handleSubmit} />
+            </div>
+        </div>
+    );
 }
