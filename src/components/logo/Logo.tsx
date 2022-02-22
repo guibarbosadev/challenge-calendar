26f646a529b28d0ceb7e1df561f3e87a34784c80
Guilherme Barbosa <me@guibarbosa.dev>
2022-02-22 20:26:34 -0300

    Create some logo image :sparkles:

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 const Logo: React.FC = () => {
-    return <h1 className={styles.logo}>Streak Calendar</h1>;
+    return <div className={styles.logo}>
+      <div className={styles.logo__calendar}>
+        <div className={styles.logo__calendar__month}>
+          Jan
+        </div>
+        <div className={styles.logo__calendar__day}>
+          01
+        </div>
+      </div>
+      <h1 className={styles.logo__text}>Streak Calendar</h1>
+    </div>
 };
 
 export default Logo;
