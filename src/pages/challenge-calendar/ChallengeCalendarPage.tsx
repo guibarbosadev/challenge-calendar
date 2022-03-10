b26a9a78dbcfa4e0c8bee9718233cb1b3a3bd7dc
Guilherme Barbosa <me@guibarbosa.dev>
2022-03-09 22:22:50 -0300

    Use react-select instead pure html select :heavy_plus_sign:

--- src/pages/challenge-calendar/ChallengeCalendarPage.tsx
+++ src/pages/challenge-calendar/ChallengeCalendarPage.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from '../../components/logo/Logo';
 import { useAppSelector } from '../../stores/hooks';
 import classNames from './styles.module.scss';
+import Select from 'react-select';
 
 const ChallengeCalendarPage: React.FC = () => {
     const { challenges } = useAppSelector((state) => state.challenge);
-    const [firstChallenge] = challenges;
-    const [selectedChallengeId, setSelectedChallengeId] = React.useState(firstChallenge.id);
-
-    const onSelectChallenge = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedChallengeId(event.target.value);
-    };
+    const options = challenges.map((challenge) => ({
+        value: challenge.id,
+        label: challenge.name
+    }));
+    const [firstOption] = options;
+    const [selectedOption, setSelectedOption] = React.useState(firstOption);
 
     return (
         <>
@@ -19,15 +20,13 @@ const ChallengeCalendarPage: React.FC = () => {
                 <button className={classNames.header__button}>Create new challenge</button>
             </header>
             <main className={classNames.body}>
-                <select value={selectedChallengeId} onChange={onSelectChallenge}>
-                    {challenges.map((challenge) => {
-                        return (
-                            <option key={challenge.id} value={challenge.id}>
-                                {challenge.name}
-                            </option>
-                        );
-                    })}
-                </select>
+                <Select
+                    className={classNames.select}
+                    value={selectedOption}
+                    options={options}
+                    onChange={(option) => option && setSelectedOption(option)}
+                    isSearchable={false}
+                />
             </main>
         </>
     );
